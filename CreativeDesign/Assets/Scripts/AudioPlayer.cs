64a154f89d7dbea1a8e198d042ff76f18f115f2f
jpaivarinta <jouka.paivarinta@gmail.com>
2020-10-21 12:00:57 +0300

    "Unity project added"

---
+++ CreativeDesign/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+    AudioSource audiosource;
+    bool playAudio = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        StartCoroutine(SoundOutput());
+    }
+
+    IEnumerator SoundOutput()
+    {
+        while (playAudio)
+        {
+            audiosource.Play();
+            yield return new WaitForSeconds(15);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
